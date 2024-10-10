19a4e0550bbe4d31592df3676a1b427d7ea42869
mnjammy007 <naseem4954r@gmail.com>
2024-10-10 17:17:14 +0530

    Initial Commit

---
+++ LOTRConverter18/LOTRConverter18App.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverter18App.swift
+//  LOTRConverter18
+//
+//  Created by Apple on 10/10/24.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverter18App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LOTRConverter18Tests/LOTRConverter18Tests.swift
@@ -0,0 +1,16 @@
+//
+//  LOTRConverter18Tests.swift
+//  LOTRConverter18Tests
+//
+//  Created by Apple on 10/10/24.
+//
+
+import Testing
+
+struct LOTRConverter18Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
